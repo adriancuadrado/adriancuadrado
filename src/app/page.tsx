c2acc4d493a69e8746e812276e628ce7359ab008
adriancuadrado <29214635+adriancuadrado@users.noreply.github.com>
2025-05-29 00:20:00 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { POSTS } from "@/utils";
+import Link from "next/link";
 
 export default async function Page() {
   return (
@@ -9,26 +10,28 @@ export default async function Page() {
       <main className="flex flex-row">
         <section className="flex flex-col gap-2">
           <div className="flex flex-col border">
-            <a
+            <Link
               className="hover:underline"
               href="https://forms.gle/TgX92fPt3KQpbgnKA"
             >
               Newsletter
-            </a>
-            <a className="hover:underline" href="rss.xml">
+            </Link>
+            <Link className="hover:underline" href="rss.xml">
               RSS feed
-            </a>
-            {/* <a className="hover:underline text-red-500" href="">
+            </Link>
+            {/* <Link className="hover:underline text-red-500" href="">
               Notifications
-            </a> */}
+            </Link> */}
           </div>
         </section>
         <section className="grow flex flex-col items-center">
           {POSTS.map((post) => (
-            <article key={post}>
-              <h1 className="font-bold text-4xl">{post}</h1>
-              <p>{post}</p>
-            </article>
+            <Link href={post} key={post}>
+              <article>
+                <h1 className="font-bold text-4xl">{post}</h1>
+                <p>{post}</p>
+              </article>
+            </Link>
           ))}
         </section>
         <section className="flex flex-col gap-2">
@@ -36,60 +39,60 @@ export default async function Page() {
             <h2 className="font-bold text-2xl">Social networks</h2>
             <ul>
               <li>
-                <a
+                <Link
                   className="hover:underline"
                   href="https://www.linkedin.com/in/adrian-cuadrado-chavarria/"
                 >
                   Linkedin
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   className="hover:underline"
                   href="https://www.twitch.tv/adriancuad"
                 >
                   Twitch
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   className="hover:underline"
                   href="https://youtube.com/@adriancuad?si=EBtiurApBIvTkXN6"
                 >
                   YouTube
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   className="hover:underline"
                   href="https://x.com/adrcuadchav97"
                 >
                   Twitter / X
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   className="hover:underline"
                   href="https://bsky.app/profile/adriancuad.bsky.social"
                 >
                   Bluesky
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   className="hover:underline"
                   href="https://www.reddit.com/user/AdrianCuad/"
                 >
                   Reddit
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   className="hover:underline"
                   href="https://github.com/adriancuadrado/"
                 >
                   Github
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
