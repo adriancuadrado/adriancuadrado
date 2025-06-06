3e6cb9ba39b738af14ac05ab673daf0195df328f
adriancuadrado <29214635+adriancuadrado@users.noreply.github.com>
2025-06-06 05:32:25 +0200

    .

--- src/components/SocialNetworks.tsx
+++ src/components/SocialNetworks.tsx
@@ -1,50 +1,65 @@
+import Image from "next/image";
 import Link from "next/link";
+
+import BlueskyLogo from "@/logos/bluesky.svg";
+import GithubLogo from "@/logos/github.svg";
+import LinkedinLogo from "@/logos/linkedin.svg";
+import RedditLogo from "@/logos/reddit.svg";
+import TwitchLogo from "@/logos/twitch.svg";
+import XLogo from "@/logos/x.svg";
+import YoutubeLogo from "@/logos/youtube.svg";
 
 export default function SocialNetworks() {
   return (
-    <ul className="sticky top-[145px] flex flex-col gap-[23px]">
-      <li>
-        <Link
-          className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
-          href="https://www.linkedin.com/in/adrian-cuadrado-chavarria/"
-        />
-      </li>
-      <li>
-        <Link
-          className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
-          href="https://www.twitch.tv/adriancuad"
-        />
-      </li>
-      <li>
-        <Link
-          className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
-          href="https://youtube.com/@adriancuad?si=EBtiurApBIvTkXN6"
-        />
-      </li>
-      <li>
-        <Link
-          className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
-          href="https://x.com/adrcuadchav97"
-        />
-      </li>
-      <li>
-        <Link
-          className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
-          href="https://bsky.app/profile/adriancuad.bsky.social"
-        />
-      </li>
-      <li>
-        <Link
-          className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
-          href="https://www.reddit.com/user/AdrianCuad/"
-        />
-      </li>
-      <li>
-        <Link
-          className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
-          href="https://github.com/adriancuadrado/"
-        />
-      </li>
-    </ul>
+    <div className="sticky top-[145px] flex flex-col gap-[23px]">
+      <Link
+        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        href="https://www.linkedin.com/in/adrian-cuadrado-chavarria/"
+      >
+        <Image src={BlueskyLogo} alt="" />
+      </Link>
+
+      <Link
+        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        href="https://www.twitch.tv/adriancuad"
+      >
+        <Image src={GithubLogo} alt="" />
+      </Link>
+
+      <Link
+        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        href="https://youtube.com/@adriancuad?si=EBtiurApBIvTkXN6"
+      >
+        <Image src={LinkedinLogo} alt="" />
+      </Link>
+
+      <Link
+        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        href="https://x.com/adrcuadchav97"
+      >
+        <Image src={RedditLogo} alt="" />
+      </Link>
+
+      <Link
+        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        href="https://bsky.app/profile/adriancuad.bsky.social"
+      >
+        <Image src={TwitchLogo} alt="" />
+      </Link>
+
+      <Link
+        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        href="https://www.reddit.com/user/AdrianCuad/"
+      >
+        <Image src={XLogo} alt="" />
+      </Link>
+
+      <Link
+        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        href="https://github.com/adriancuadrado/"
+      >
+        <Image src={YoutubeLogo} alt="" />
+      </Link>
+    </div>
   );
 }
