95a1754a34723df4ce30dfbf64f63c07512fe94f
adriancuadrado <29214635+adriancuadrado@users.noreply.github.com>
2025-06-06 05:49:38 +0200

    .

--- src/components/SocialNetworks.tsx
+++ src/components/SocialNetworks.tsx
@@ -13,49 +13,57 @@ export default function SocialNetworks() {
   return (
     <div className="sticky top-[145px] flex flex-col gap-[23px]">
       <Link
-        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        className="hover:underline flex justify-center bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] size-[37px] rounded-3xl"
         href="https://www.linkedin.com/in/adrian-cuadrado-chavarria/"
       >
-        <Image src={BlueskyLogo} alt="" />
+        <Image
+          src={BlueskyLogo}
+          alt=""
+          className="transform-[scale(0.7)_translate(0,3px)]"
+        />
       </Link>
 
       <Link
-        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        className="hover:underline flex justify-center bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] size-[37px] rounded-3xl"
         href="https://www.twitch.tv/adriancuad"
       >
         <Image src={GithubLogo} alt="" />
       </Link>
 
       <Link
-        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        className="hover:underline flex justify-center bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] size-[37px] rounded-3xl"
         href="https://youtube.com/@adriancuad?si=EBtiurApBIvTkXN6"
       >
         <Image src={LinkedinLogo} alt="" />
       </Link>
 
       <Link
-        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        className="hover:underline flex justify-center bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] size-[37px] rounded-3xl"
         href="https://x.com/adrcuadchav97"
       >
         <Image src={RedditLogo} alt="" />
       </Link>
 
       <Link
-        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        className="hover:underline flex justify-center bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] size-[37px] rounded-3xl"
         href="https://bsky.app/profile/adriancuad.bsky.social"
       >
-        <Image src={TwitchLogo} alt="" />
+        <Image
+          src={TwitchLogo}
+          alt=""
+          className="transform-[scale(0.6)_translate(1px,3px)]"
+        />
       </Link>
 
       <Link
-        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        className="hover:underline flex justify-center bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] size-[37px] rounded-3xl"
         href="https://www.reddit.com/user/AdrianCuad/"
       >
-        <Image src={XLogo} alt="" />
+        <Image src={XLogo} alt="" className="transform-[scale(0.6)]" />
       </Link>
 
       <Link
-        className="hover:underline bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] block size-[37px] rounded-3xl"
+        className="hover:underline flex justify-center bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] size-[37px] rounded-3xl"
         href="https://github.com/adriancuadrado/"
       >
         <Image src={YoutubeLogo} alt="" />
